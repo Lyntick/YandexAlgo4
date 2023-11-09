eb2e54bb942070b9e4d066b3092760e2758dda5a
artsiomzasinets <artsiomzasinets@gmail.com>
2023-11-09 22:15:44 +0300

    Task 2 has been done

--- Task2/main.cpp
+++ Task2/main.cpp
@@ -1,35 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <random>
+#include <fstream>
 
-void quickSort(std::vector<int>& buf, int l, int r){
+
+std::default_random_engine generator;
+
+void quickSort(std::vector<int>& buf, int l, int r){// r = N - 1
     if(l < r){
-        //partition
-        int mid = l/2 + r/2;
+        std::uniform_int_distribution<int> distribution(l,r);
+        int mid = distribution(generator);
         int supElem = buf[mid];
-        while(buf[l] < supElem){++l;}
-        while(supElem < buf[r]){--r;}
-        if(l >= r){
-            return;
-        }
-        std::swap(buf[l], buf[mid]);// move supElem at the beginning
-        //Solution
-        int less = l, equal = l+1, bigger = l+1;
+
+        int less = l, equal = l, bigger = l;
         do{
-            int tempBig = buf[bigger];
-            if(tempBig < supElem){
+            int temp = buf[bigger];
+            if(temp < supElem){
                 buf[bigger] = buf[equal];
                 buf[equal] = buf[less];
-                buf[less] = tempBig;
+                buf[less] = temp;
                 ++equal;
                 ++less;
-            }else if(tempBig == supElem){
+            }else if(temp == supElem) {
                 std::swap(buf[bigger], buf[equal]);
                 ++equal;
             }
             ++bigger;
-        }while(bigger < r);
-        //
-        quickSort(buf, l,less-1);
+        }while(bigger <= r);
+
+        quickSort(buf, l,less);
         quickSort(buf,equal, r);
     }
 }
@@ -42,18 +41,39 @@ int main() {
     std::vector<int> buf;
     int N = -1;
 
-        std::cin >> N;
-        buf.reserve(N);
-        for(int i = 0; i < N; ++i) {
-            std::cin >> buf[i];
-        }
-    //Solution
-    quickSort(buf, 0, N);
-    //
-    for(int i = 0; i < N; ++i){
-        std::cout << buf[i] << ' ';
-    }
+//        std::cin >> N;
+//        buf.reserve(N);
+//        for(int i = 0; i < N; ++i) {
+//            std::cin >> buf[i];
+//        }
 
+//    //Solution
+//    quickSort(buf, 0, N);
+//    //
+//    for(int i = 0; i < N; ++i){
+//        std::cout << buf[i] << ' ';
+//    }
+
+//    N = 4000000;
+//    srand(time(0));
+//    for(int i = 0; i < N; ++i){
+//        auto temp = rand() % 10000000;
+//        buf.push_back(temp);
+//    }
+//    std::cout << '\n';
+//
+
+    auto start = std::chrono::steady_clock::now();
+    quickSort(buf,0, N);
+//    std::sort(buf.begin(),buf.end());
+    auto end = std::chrono::steady_clock::now();
+    std::cout << "work time:" << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    for(int i =0; i < N-1; ++i){
+        if(buf[i] > buf[i+1]){
+            std::cout << "\nfalse\n";
+            break;
+        }
+    }
 
     return 0;
 }
